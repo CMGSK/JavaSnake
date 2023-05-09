6402b7042c8ace72116d7d97cb0bf2431cd64431
CMGSK <carlosmgov7k@gmail.com>
2023-05-09 18:10:15 +0200

    FUll game implementation. Bug fix pending. Lacking menu interface

--- src/Board.java
+++ src/Board.java
@@ -13,18 +13,18 @@ import javax.swing.Timer;
 public class Board extends JPanel implements ActionListener{
 
 	//screen parameters
-	static final int WIDTH = 600;
-	static final int HEIGHT = 400;
-	static final int USIZE = 17; //Unit size
+	static final int USIZE = 15; //Unit size
+	static final int WIDTH = USIZE*50;
+	static final int HEIGHT = USIZE*30 -1;
 	static final int UNITS = (int) ((WIDTH*HEIGHT)/(Math.pow(USIZE, 2)));
 	
-	static final int DELAY = 100; //the lower, the fastest
+	static final int DELAY = 60; //the lower, the fastest
 	
 	final int[] x = new int[UNITS]; //x body parts
 	final int[] y = new int[UNITS]; //y body parts
 	
 	int body = 6; //body length
-	char direction = 'D'; // W(Up)  A(Left) S(Down) D(Right)
+	char direction = 'd'; // W(Up)  A(Left) S(Down) D(Right)
 	
 	int score;
 
@@ -62,20 +62,31 @@ public class Board extends JPanel implements ActionListener{
 	
 	public void draw(Graphics g) {
 		
-		//Helping grid
-		for (int i=0; i<WIDTH/USIZE; i++) {
-			g.drawLine(i*USIZE, 0, i*USIZE, HEIGHT); //draw some vertical lines
-			g.drawLine(0, i*USIZE, WIDTH, i*USIZE); //draw some horizontal lines
-		}
-		
-		//Color food then place it on XY with 1x1 usize;
-		g.setColor(Color.RED);
-		g.fillOval(foodX, foodY, USIZE, USIZE);
-		
+		if (running){
+		  //Helping grid
+			for (int i=0; i<WIDTH/USIZE; i++) {
+				g.drawLine(i*USIZE, 0, i*USIZE, HEIGHT); //draw some vertical lines
+				g.drawLine(0, i*USIZE, WIDTH, i*USIZE); //draw some horizontal lines
+			}
+		  
+		  //Color food then place it on XY with 1x1 usize;
+			g.setColor(Color.RED);
+			g.fillOval(foodX, foodY, USIZE, USIZE);
+
+		  //Color the snake body and head
+			for (int i = 0; i<body; i++){
+				if (i==0){
+					g.setColor(Color.GREEN);
+					g.fillRect(x[i], y[i], USIZE, USIZE);
+				}else{
+					g.setColor(new Color(20, 180, 40));
+					g.fillRect(x[i], y[i], USIZE, USIZE);
+				}
+			}
+		} else gameOver(g);
 	}
 	
 	public void move () {
-		
 		//each part of the body will switch to the previous part location, starting on top position (tail)
 		for (int i=body; i>0; i--) {
 			x[i] = x[i-1]; 
@@ -84,22 +95,19 @@ public class Board extends JPanel implements ActionListener{
 		
 		//we only move the head here, and the rest are supposed to follow
 		switch (direction) {
-		case 'w':
-			y[0] = y[0] - USIZE;
-			break;
-		case 's':
-			y[0] = y[0] + USIZE;
-			break;
-		case 'a':
-			x[0] = x[0] - USIZE;
-			break;
-		case 'd':
-			x[0] = x[0] + USIZE;
-			break;
-			
-			
+			case 'w':
+				y[0] = y[0] - USIZE;
+				break;
+			case 's':
+				y[0] = y[0] + USIZE;
+				break;
+			case 'a':
+				x[0] = x[0] - USIZE;
+				break;
+			case 'd':
+				x[0] = x[0] + USIZE;
+				break;
 		}
-		
 	}
 	
 	public void popFood() {
@@ -107,29 +115,60 @@ public class Board extends JPanel implements ActionListener{
 		foodY = random.nextInt((int)HEIGHT/USIZE)*USIZE;
 	}
 	
-	public void isFood() {
-		
+	public void eat() {
+		if (x[0]==foodX && y[0]==foodY){
+		  body *= 1.3;
+		  score += body * 10;
+		  popFood();
+		}
 	}
 	
 	public void collide() {
+		//if head joins body
+		for (int i = body; i > 0; i--) if ((x[0] == x[i]) && (y[0] == y[i])) running=false;
 		
+		//if head touches Left/Right
+		//if head touches Top/Bottom
+		if (x[0] < 0 || x[0] > WIDTH) running = false;
+		if (y[0] < 0 || y[0] > HEIGHT) running = false;
+		
+		//Stop the program if there was a collision
+		if (!running) timer.stop();
 	}
 	
 	public void gameOver(Graphics g) {
-		
+		String res = "Game over!\n\nYour Score:\n" + score;
+		g.drawString(res, WIDTH/2, HEIGHT/2);
 	}
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		
+		if (running){
+		  move();
+		  eat();
+		  collide();
+		}
+		repaint();
 	}
 
 	public class Keys extends KeyAdapter {
 		@Override
 		public void keyPressed(KeyEvent e) {
-			
+
+			switch(e.getKeyCode()){
+				case KeyEvent.VK_W:
+				  if (direction != 's') direction = 'w';
+				  break;
+				case KeyEvent.VK_S:
+				  if (direction != 'w') direction = 's';
+				  break;
+				case KeyEvent.VK_D:
+				  if (direction != 'a') direction = 'd';
+				  break;
+				case KeyEvent.VK_A:
+				  if (direction != 'd') direction = 'a';
+				  break;
+			}
 		}
 	}
-
 }
